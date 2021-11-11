3c6237d96c918f075908fbfa69b2525ab09ad7a6
Caleb Hatch <hatchcaleb@gmail.com>
2021-11-11 14:44:35 -0600

    Getting inputs from a button

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,32 @@
 $(document).ready(handleReady);
 
 function handleReady() {
-  console.log("jquery is loaded!")
-}
+  console.log("jquery is loaded!");
+  $('#submitButton').on('click', submitGuesses);
+};
+
+function submitGuesses() {
+let guessRound = {
+  playerOne: $('#player1').val(),
+  playerTwo: $('#player2').val(),
+  playerThree: $('#player3').val(),
+  playerFour: $('#player4').val()
+};
+$.ajax({
+  method: 'POST',
+  url: '/guesses',
+  data: guessRound
+}).then((res) => {
+  console.log('It worked');
+  
+}).catch((error) => {
+  console.log('FAILED');
+})
+}; // end submitGuesses
+
+function clearInputs(){
+  $('#player1').val('');
+  $('#player2').val('');
+  $('#player3').val('');
+  $('#player4').val('');
+}; // end clearInputs
--- server/server.js
+++ server/server.js
@@ -1,3 +1,10 @@
+let guessArray = [
+  {playerOne: 'GuessOne'},
+  {playerTwo: 'GuessTwo'},
+  {playerThree: 'GuessThree'},
+  {playerFour: 'GuessFour'}
+]; // end guessArray
+
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
@@ -9,8 +16,20 @@ app.use(bodyParser.urlencoded({extended:true}))
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
-// GET & POST Routes go here
+let randomNumberGenerator = require('./modules/randomNumber.js');
 
+// GET & POST Routes go here
+app.get('/guesses', (req, res) => {
+  console.log('in get /guesses');
+  res.send(guessArray);
+}); // end get /randomNumber
+
+app.post('/guesses', (req, res) => {
+  console.log('in post /guesses');
+  console.log('req.body', req.body);
+  guessArray.push(req.body);
+  res.sendStatus(201);
+})
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
